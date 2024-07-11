2d2dd2ba755f6cc64ef2074a866a277cb8759514
hamzafz7 <hamzafz888@gmail.com>
2024-07-11 14:13:18 +0300

    first push

--- lib/core/utils/app_consts.dart
+++ lib/core/utils/app_consts.dart
@@ -1,5 +1,5 @@
 import 'package:get_storage/get_storage.dart';
 
-const baseUrl = "https://ryadeapp.technoplus.dev/api/";
+const baseUrl = " ";
 const imageBaseUrl = "https://ryadeapp.technoplus.dev/storage/";
 final box = GetStorage();
